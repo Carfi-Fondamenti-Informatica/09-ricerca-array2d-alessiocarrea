3f269e7111ecf3244d13536a55cdac669f9dec86
alessiocarrea <118049896+alessiocarrea@users.noreply.github.com>
2022-12-15 22:24:03 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
+  char array[10][20];
+  char array2[20];
+  int pos=0;
+      for (int i=0; i<10; i++) {
+      for (int j=0; j<20; j++) {
+        array[i][j]=' ';
+        array2[j]= ' ';
+      }
+      }
+  for (int=0; int<10; i++){
+cin >> array[i];
+  }
+cin >> array2;
+  
+  if (funzione (array, array2, pos) {
+    cout << pos;
+  } else {cout << "non presente";}
+      
   return 0;
 }
