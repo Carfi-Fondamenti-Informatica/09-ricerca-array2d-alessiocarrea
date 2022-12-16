3994b0acfc46edad9b5e7ddfe111d82055af792a
alessiocarrea <118049896+alessiocarrea@users.noreply.github.com>
2022-12-16 09:43:18 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,4 @@
-bool funzione (char Array[10][20], char Array2[20], int &pos);
+#ifndef UNTITLED15_LIB_H
+#define UNTITLED15_LIB_H
+bool funzione (char Array[10][20]; char Array2[20]; int &pos);
+#endif //UNTITLED15_LIB_H
