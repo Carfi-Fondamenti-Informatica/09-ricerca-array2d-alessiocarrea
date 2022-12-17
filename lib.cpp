937729800700f0620dfd40e4e536d7fb574d780b
alessiocarrea <118049896+alessiocarrea@users.noreply.github.com>
2022-12-17 11:52:03 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -3,7 +3,7 @@
 int funzione (char lista[10][20], char nome[20]) {
     int a=0;
     int b=0;
-    while ((b < 19) and (a < 10)) {
+    while ((b < 19),(a < 10)) {
         if (lista[a][b]! = nome[b]) {
             a++;
             b = 0;
