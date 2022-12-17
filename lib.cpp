14bc2581127d366492a552dbc02ff9e933253f77
alessiocarrea <118049896+alessiocarrea@users.noreply.github.com>
2022-12-17 11:20:36 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,15 +1,15 @@
 #include "lib.h"
 
-bool funzione (char Array[10][20]; char Array2[20]; int &pos) {
-  for (int i=0; i<10; i++) {
-      for (int j=0; j<20; j++) {
-         if (Array2[j] !=Array[i][j]) {
-           j=20;
-         } else if (j==19) {
-             pos=i;
-             return true;
-         }
-      }
-  }
-  return false;
+bool funzione (char Array1[10][20], char Array2[20], int &pos) {
+for (int a=0; a<10; a++) {
+for (int b=0; b<20; b++) {
+if (Array2[b]!=Array1[a][b]) {
+b=20;
+} else if (b==19) {
+pos=a;
+return true;
+}
+}
+}
+return false;
 }
